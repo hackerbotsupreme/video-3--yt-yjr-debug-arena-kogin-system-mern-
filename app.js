847427139e89a72bdcd7e0370d60cc86e0bf0526
Aloke Pramanik <129755834+hackerbotsupreme@users.noreply.github.com>
2023-11-08 14:39:53 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,261 +1,59 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-app.use(express.json());
-const cors = require("cors");
-app.use(cors());
-const bcrypt = require("bcryptjs");
-app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: false }));
+// we will import the dependencies as variables to use them
+// to start the server at 5000 we need to add script in the package.json 
+// "start":"node app.js"
+// then run nodemon app
 
-const jwt = require("jsonwebtoken");
-var nodemailer = require("nodemailer");
+const express = require("express")
+const  app = express()
 
-const JWT_SECRET =
-  "hvdvay6ert72839289()aiyg8t87qt72393293883uhefiuh78ttq3ifi78272jbkj?[]]pou89ywe";
+app.listen(5000,()=>{
+    console.log('server staarted at the port 5000')
+})
+// started the server 
 
-const mongoUrl =
-  "mongodb+srv://adarsh:adarsh@cluster0.zllye.mongodb.net/?retryWrites=true&w=majority";
+// making an api 
+// to make an api we have many methods like post , fetch , delete ,
+// we are going to make an basic api through which awe can send request and receive a response 
+//  so that we can figure out how api works 
 
-mongoose
-  .connect(mongoUrl, {
-    useNewUrlParser: true,
-  })
-  .then(() => {
-    console.log("Connected to database");
-  })
-  .catch((e) => console.log(e));
+// ...........................................................................................................................................................
+// Here is a high-level overview of several types of API methods:
 
-require("./userDetails");
-require("./imageDetails");
+// GET method: retrieves information or data from a specified resource
+// POST method: submits data to be processed to a specified resource
+// PUT method: updates a specified resource with new data
+// DELETE method: deletes a specified resource
+// PATCH method: partially updates a specified resource
+// OPTIONS method: retrieves the supported HTTP methods of a server endpoint
+// HEAD method: retrieves only the headers of a response without the response body
+// CONNECT method: establishes a network connection to a resource, typically used for SSL/TLS tunneling
+// TRACE method: echoes the received request back to the client, for debugging purposes
+// API methods are typically used to interact with web services or web applications, allowing developers to access and manipulate data or functionality from a remote source.
+// ...........................................................................................................................................................
 
-const User = mongoose.model("UserInfo");
-const Images = mongoose.model("ImageDetails");
-app.post("/register", async (req, res) => {
-  const { fname, lname, email, password, userType } = req.body;
-
-  const encryptedPassword = await bcrypt.hash(password, 10);
-  try {
-    const oldUser = await User.findOne({ email });
-
-    if (oldUser) {
-      return res.json({ error: "User Exists" });
-    }
-    await User.create({
-      fname,
-      lname,
-      email,
-      password: encryptedPassword,
-      userType,
-    });
-    res.send({ status: "ok" });
-  } catch (error) {
-    res.send({ status: "error" });
-  }
-});
-
-app.post("/login-user", async (req, res) => {
-  const { email, password } = req.body;
-
-  const user = await User.findOne({ email });
-  if (!user) {
-    return res.json({ error: "User Not found" });
-  }
-  if (await bcrypt.compare(password, user.password)) {
-    const token = jwt.sign({ email: user.email }, JWT_SECRET, {
-      expiresIn: "15m",
-    });
-
-    if (res.status(201)) {
-      return res.json({ status: "ok", data: token });
-    } else {
-      return res.json({ error: "error" });
-    }
-  }
-  res.json({ status: "error", error: "InvAlid Password" });
-});
-
-app.post("/userData", async (req, res) => {
-  const { token } = req.body;
-  try {
-    const user = jwt.verify(token, JWT_SECRET, (err, res) => {
-      if (err) {
-        return "token expired";
-      }
-      return res;
-    });
-    console.log(user);
-    if (user == "token expired") {
-      return res.send({ status: "error", data: "token expired" });
-    }
-
-    const useremail = user.email;
-    User.findOne({ email: useremail })
-      .then((data) => {
-        res.send({ status: "ok", data: data });
-      })
-      .catch((error) => {
-        res.send({ status: "error", data: error });
-      });
-  } catch (error) { }
-});
-
-app.listen(5000, () => {
-  console.log("Server Started");
-});
-
-app.post("/forgot-password", async (req, res) => {
-  const { email } = req.body;
-  try {
-    const oldUser = await User.findOne({ email });
-    if (!oldUser) {
-      return res.json({ status: "User Not Exists!!" });
-    }
-    const secret = JWT_SECRET + oldUser.password;
-    const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, {
-      expiresIn: "5m",
-    });
-    const link = `http://localhost:5000/reset-password/${oldUser._id}/${token}`;
-    var transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: "adarsh438tcsckandivali@gmail.com",
-        pass: "rmdklolcsmswvyfw",
-      },
-    });
-
-    var mailOptions = {
-      from: "youremail@gmail.com",
-      to: "thedebugarena@gmail.com",
-      subject: "Password Reset",
-      text: link,
-    };
-
-    transporter.sendMail(mailOptions, function (error, info) {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log("Email sent: " + info.response);
-      }
-    });
-    console.log(link);
-  } catch (error) { }
-});
-
-app.get("/reset-password/:id/:token", async (req, res) => {
-  const { id, token } = req.params;
-  console.log(req.params);
-  const oldUser = await User.findOne({ _id: id });
-  if (!oldUser) {
-    return res.json({ status: "User Not Exists!!" });
-  }
-  const secret = JWT_SECRET + oldUser.password;
-  try {
-    const verify = jwt.verify(token, secret);
-    res.render("index", { email: verify.email, status: "Not Verified" });
-  } catch (error) {
-    console.log(error);
-    res.send("Not Verified");
-  }
-});
-
-app.post("/reset-password/:id/:token", async (req, res) => {
-  const { id, token } = req.params;
-  const { password } = req.body;
-
-  const oldUser = await User.findOne({ _id: id });
-  if (!oldUser) {
-    return res.json({ status: "User Not Exists!!" });
-  }
-  const secret = JWT_SECRET + oldUser.password;
-  try {
-    const verify = jwt.verify(token, secret);
-    const encryptedPassword = await bcrypt.hash(password, 10);
-    await User.updateOne(
-      {
-        _id: id,
-      },
-      {
-        $set: {
-          password: encryptedPassword,
-        },
-      }
-    );
-
-    res.render("index", { email: verify.email, status: "verified" });
-  } catch (error) {
-    console.log(error);
-    res.json({ status: "Something Went Wrong" });
-  }
-});
-
-app.get("/getAllUser", async (req, res) => {
-  try {
-    const allUser = await User.find({});
-    res.send({ status: "ok", data: allUser });
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-app.post("/deleteUser", async (req, res) => {
-  const { userid } = req.body;
-  try {
-    User.deleteOne({ _id: userid }, function (err, res) {
-      console.log(err);
-    });
-    res.send({ status: "Ok", data: "Deleted" });
-  } catch (error) {
-    console.log(error);
-  }
-});
+// get method - in the get method  we can only receive a response from a server 
+// post method  - in teh post , we can send a request and receive a response 
 
 
-app.post("/upload-image", async (req, res) => {
-  const { base64 } = req.body;
-  try {
-    await Images.create({ image: base64 });
-    res.send({ Status: "ok" })
 
-  } catch (error) {
-    res.send({ Status: "error", data: error });
-
-  }
+// ok so when user will click submit , the website will request my api that entered data 
+app.post("/post",async (req,res)=>{
+    // we will receive that data using  console.log(res.body)
+    console.log(res.body)
 })
 
-app.get("/get-image", async (req, res) => {
-  try {
-    await Images.find({}).then(data => {
-      res.send({ status: "ok", data: data })
-    })
+// use postman api to make api calls 
+// is post man api is used to build efficient api's ?
+// Organizations who prioritize API management typically leverage an API platform 
+// like Postman, which can help them design, develop, test, secure, deploy, and monitor APIs at scale.
 
-  } catch (error) {
+// it's how we create a whole fully functional website 
+// Setting up a server and folder structure is essential for organizing your project.
+// Prioritize UX/UI design with HTML, CSS, and possibly Sass to create an appealing user interface.
+// In the backend, create the API (middleware) to handle data and functionality. This separation of frontend and backend is a best practice for web development.
+// Thoroughly test the API to ensure it functions correctly. Testing is crucial to avoid issues down the line.
+// You're correct about using a backend database like SQL or MongoDB to store data securely.
+// Finally, hosting your website is the last step in making it accessible to the world.
+// This approach aligns with web development best practices and will help you create a fully functional website.
+// that's how we create a whole fully-dynamic  functional website 
 
-  }
-})
-
-app.get("/paginatedUsers", async (req, res) => {
-  const allUser = await User.find({});
-  const page = parseInt(req.query.page)
-  const limit = parseInt(req.query.limit)
-
-  const startIndex = (page - 1) * limit
-  const lastIndex = (page) * limit
-
-  const results = {}
-  results.totalUser=allUser.length;
-  results.pageCount=Math.ceil(allUser.length/limit);
-
-  if (lastIndex < allUser.length) {
-    results.next = {
-      page: page + 1,
-    }
-  }
-  if (startIndex > 0) {
-    results.prev = {
-      page: page - 1,
-    }
-  }
-  results.result = allUser.slice(startIndex, lastIndex);
-  res.json(results)
-})
